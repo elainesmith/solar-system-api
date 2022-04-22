5f2f59def6ecd90b9f403d22a3567345d861dbd0
Jodi Denney <iarrived@gmail.com>
2022-04-22 12:07:41 -0700

    Passed Wave 1 - Planet class, GET planets API

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,27 @@
-from flask import Blueprint
+from unicodedata import name
+from flask import Blueprint, jsonify
 
+
+class Planet():
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+
+planets = [Planet(1, "Mercury", "blue"),
+           Planet(2, "Venus", "round"),
+           Planet(3, "PizzaPlanet", "pepperoni")]
+
+planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
+
+
+@planet_bp.route("", methods=["GET"])
+def get_all_planets():
+    planet_reply = []
+    for planet in planets:
+        planet_reply.append({"id": planet.id,
+                             "name": planet.name,
+                             "description": planet.description})
+
+    return jsonify(planet_reply)
