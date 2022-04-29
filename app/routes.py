88aca6243372ba932ad5e425522f13838447965d
Jodi Denney <iarrived@gmail.com>
2022-04-29 12:20:22 -0700

    Added db and migrate, Created PLanet Model, Post route

--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,47 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.planet import Planet
 
-
-class Planet():
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
-
-
-planets = [Planet(1, "Mercury", "blue"),
-           Planet(2, "Venus", "round"),
-           Planet(3, "PizzaPlanet", "pepperoni")]
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
 
-@planet_bp.route("", methods=["GET"])
-def get_all_planets():
-    planet_reply = []
-    for planet in planets:
-        planet_reply.append({"id": planet.id,
-                             "name": planet.name,
-                             "description": planet.description})
+@planet_bp.route("", methods=["POST"])
+def create_one_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        descr=request_body['descr'],
+                        num_of_starbucks=request_body["num_of_starbucks"])
 
-    return jsonify(planet_reply)
+    db.session.add(new_planet)
+    db.session.commit()
 
-@planet_bp.route("<planet_id>", methods=["GET"])
-def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        return {"msg": f"Planet ID must be numerical: {planet_id}"}, 400
-    
-    for planet in planets:
-        if planet.id == planet_id:
-            return {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description
-            }
-    return {"msg": f"Planet ID not found: {planet_id}"}, 404
+    return make_response(f"Planet {new_planet.name} succesfully created.  Good job spaceman!", 201)
+
+
+# @planet_bp.route("", methods=["GET"])
+# def get_all_planets():
+#     planet_reply = []
+#     for planet in planets:
+#         planet_reply.append({"id": planet.id,
+#                              "name": planet.name,
+#                              "description": planet.description})
+
+#     return jsonify(planet_reply)
+
+
+# @planet_bp.route("<planet_id>", methods=["GET"])
+# def get_one_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         return {"msg": f"Planet ID must be numerical: {planet_id}"}, 400
+
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description
+#             }
+#     return {"msg": f"Planet ID not found: {planet_id}"}, 404
